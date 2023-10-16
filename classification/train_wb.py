26358d6f543353e0bea80ea5d0c42ce0829d0079
SanchoPanso <20alexchernik@gmail.com>
2023-10-17 00:05:58 +0200

    feat: add pretrained

--- classification/train_wb.py
+++ classification/train_wb.py
@@ -1,23 +1,25 @@
 import os
 import sys
+import argparse
 from pathlib import Path
-import yaml
 import torch
-from easydict import EasyDict
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
+from typing import Sequence
 
 sys.path.append(str(Path(__file__).parent))
 from train.augmentation import DataAugmentation
 from train.model import EfficientLightning
 from train.service import TrainWrapper
-import argparse
+from utils.cfg_handler import get_cfg
 
 
 def main():
     torch.cuda.empty_cache()
     args = parse_args()
-    cfg = get_cfg(args)
+    cfg = get_cfg(args.cfg)
+    cfg.update(vars(args))
+    print('cfg:', cfg)
 
     WRAPPER = TrainWrapper(
         cfg=cfg,
@@ -32,6 +34,12 @@ def main():
         augmentation=DataAugmentation,
         weights=WRAPPER.weights,
     )
+    
+    if cfg.pretrained:
+        cfg.pretrained = os.path.join(cfg.data_path, cfg.mask_dir, cfg.pretrained)
+        checkpoint = torch.load(cfg.pretrained)
+        state_dict = checkpoint['state_dict']
+        model.load_state_dict(state_dict)
 
     wandb_logger = WandbLogger(
         project=WRAPPER.cat,
@@ -44,21 +52,21 @@ def main():
     # Initialize a trainer
     trainer = Trainer(
         accelerator="gpu",
-        devices=[1],
+        devices=[cfg.gpu],
         max_epochs=cfg.epochs, # 60,
         precision=16,
         log_every_n_steps=1,
         logger=wandb_logger,
         callbacks=WRAPPER.get_callbacks(),
     )
-    
+
     # Train the model ⚡
     trainer.fit(model, WRAPPER.train_loader, WRAPPER.val_loader)
 
     path_ = WRAPPER.get_best_model(model)
 
 
-def parse_args():
+def parse_args(src_args: Sequence[str] | None = None):
     parser = argparse.ArgumentParser(
         description="Type path to: model, json, data(optional)"
     )
@@ -68,15 +76,18 @@ def parse_args():
     )
     parser.add_argument(
         "--cat", dest="cat", type=str, 
-        # default="tits_size", 
         default="body_type", 
         help="category", required=False,
+    )
+    parser.add_argument(
+        "--bg_cat", type=str, 
+        default="background", 
     )
     parser.add_argument(
         "--batch",
         dest="batch_size",
         type=int,
-        default=48,
+        default=32,
         help="Batch size",
         required=False,
     )
@@ -92,7 +103,7 @@ def parse_args():
         "--decay",
         dest="decay",
         type=float,
-        default=0.2,
+        default=0.001,
         help="Decay",
         required=False,
     )
@@ -128,7 +139,6 @@ def parse_args():
         help="Using masks instead regular images",
         required=False,
     )
-    
     parser.add_argument(
         "--epochs",
         type=int,
@@ -139,20 +149,23 @@ def parse_args():
     parser.add_argument(
         "--badlist_path",
         type=str,
-        default=os.path.join(os.path.dirname(__file__), 'data/badlist.txt'),
+        default=None, # os.path.join(os.path.dirname(__file__), 'data/badlist.txt'),
         help='Path to txt file which is a list of bad annotatated images',
     )
-    args = parser.parse_args()
-    return args
-
-
-def get_cfg(args: argparse.Namespace) -> EasyDict:
-    cfg_path = args.cfg
-    with open(cfg_path) as f:
-        cfg = yaml.load(f, yaml.Loader)
+    parser.add_argument(
+        "--pretrained",
+        type=str,
+        default=None, # 'body_type/v__6_train_eff_48_0.2/checkpoints/epoch=31-step=12320.ckpt',#None,
+    )
     
-    cfg.update(vars(args))
-    return EasyDict(cfg)
+    parser.add_argument(
+        "--gpu",
+        type=int,
+        default=0,
+    )
+    
+    args = parser.parse_args(src_args)
+    return args
     
 
 if __name__ == "__main__":
